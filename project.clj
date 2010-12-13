d5ae38b0acc4e888dfcf382787149ea8b7847ca2
Aria Haghighi <aria42@gmail.com>
2010-12-12 20:55:05 -0500

    Fixed version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject text-ann "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject text-ann "0.0.1-SNAPSHOT"
+  :description "Tool for generating text annotation pages. Focused on producing external HITs for Mechanical Turk."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
